4488a9cc500eacf67a4f8d46f606a44ee5a3d3ac
Golban-Julia <golden95juli@gmail.com>
2022-12-15 23:47:28 +0200

    Added solution

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import InvestmantCard from "./components/InvestmantCard";
+import TodosList from "./components/TodosList";
+
+function App() {
+  return (
+    <div>
+      <InvestmantCard />
+      <TodosList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/investment/actions.js
@@ -0,0 +1,17 @@
+import { DEPOSIT, WITHDRAW, RESET } from './actionTypes';
+
+
+export const deposit = (value) => ({
+    type: DEPOSIT,
+    payload: value
+});
+
+export const withdraw = (value) => ({
+    type:  WITHDRAW,
+    payload: value
+});
+
+export const reset = (value) => ({
+    type:  RESET,
+    payload: value
+});
